a46866342fa663babf76ed452ada871947f64dd1
Mans Zigher <extzig@bang-olufsen.dk>
2018-01-02 08:26:34 +0100

    Adding new quote

--- quotes1.php
+++ quotes1.php
@@ -5,8 +5,8 @@
 				<div class="col-sm-4 col-md-2 col-md-offset-2 hidden-xs">
 					<img src="images/bulbs.png" class="img-responsive">
 				</div><!-- End column -->
-				<div class="col-sm-8 col-md-6">					
-					<blockquote>Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time."<p>― Thomas A. Edison</p></blockquote>					
+				<div class="col-sm-8 col-md-6">
+					<blockquote>We must ensure that technology is accessible, affordable, and adds value.”<p>― Narendra Modi</p></blockquote>	
 				</div><!-- End column -->
 			</div><!-- End row -->
 		</div><!-- End container -->
